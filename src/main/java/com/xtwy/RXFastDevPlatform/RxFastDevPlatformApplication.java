a673dcd2a27ced327df014b2522d9266bcff3853
houjianjun <474177818@qq.com>
2018-05-14 11:08:23 +0800

    初建项目。

---
+++ src/main/java/com/xtwy/RXFastDevPlatform/RxFastDevPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.xtwy.RXFastDevPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxFastDevPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxFastDevPlatformApplication.class, args);
+	}
+}
